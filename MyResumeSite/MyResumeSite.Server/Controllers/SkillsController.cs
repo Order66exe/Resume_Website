295697029c12ad81c96356c60b840ce1e6e40a7f
Order66exe <zwslocum@yahoo.com>
2026-09-07 14:10:51 -0400

    Switch SkillsController to using SQL connection

--- MyResumeSite/MyResumeSite.Server/Controllers/SkillsController.cs
+++ MyResumeSite/MyResumeSite.Server/Controllers/SkillsController.cs
@@ -24,31 +24,31 @@ namespace MyResumeSite.Server.Controllers
         public async Task<Skill[]> Get()
         {
 
-            //var dbContext = new ResumeSiteDBContext();
-            //try
-            //{
-            //    dbContext.Database.OpenConnection();
-            //    dbContext.Database.CloseConnection();
-            //}
-            //catch (SqlException)
-            //{
-            //    Console.WriteLine("uh oh");
-            //}
-            //Skill[] skills = new Skill[5];
-            //using (var context = new ResumeSiteDBContext())
-            //{
-            //    var thing = context.Skills.AsQueryable();
-            //    string tester = thing.ToString();
-            //    var thingy = thing.ElementAt(0);
-            //    skills = thing.ToArray();
-            //}
-            //return skills;
-            Skill[] skills = new Skill[5];
-            for (int i = 0; i < skills.Length; i++)
+            var dbContext = new ResumeSiteDBContext();
+            try
             {
-                skills[i] = new Skill { Name = SkillNames[i], My_Proficiency = "Beginner", Years_Of_Experience = 1 };
+                dbContext.Database.OpenConnection();
+                dbContext.Database.CloseConnection();
+            }
+            catch (SqlException)
+            {
+                Console.WriteLine("uh oh");
+            }
+            Skill[] skills = new Skill[5];
+            using (var context = new ResumeSiteDBContext())
+            {
+                var thing = context.Skills.AsQueryable();
+                string tester = thing.ToString();
+                var thingy = thing.ElementAt(0);
+                skills = thing.ToArray();
             }
             return skills;
+            //Skill[] skills = new Skill[5];
+            //for (int i = 0; i < skills.Length; i++)
+            //{
+            //    skills[i] = new Skill { Name = SkillNames[i], My_Proficiency = "Beginner", Years_Of_Experience = 1 };
+            //}
+            //return skills;
         }
     }
 }
